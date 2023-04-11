39acfe37a5e7c0658c33613fe9bec4fe1e8f6463
ppaka <asj0216@gmail.com>
2023-04-11 10:42:12 +0900

    챌린지

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float time;
+    private float waitTime = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > waitTime)
+        {
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                time = 0;
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
+        }
+    }
+}
